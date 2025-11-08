f49e94c55699c79560982d696c69b95ca7327193
angelicaa-g <angelicaghazaryann@gmail.com>
2025-11-08 15:28:20 -0800

    added the basics

--- Homework/HW2/js/script.js
+++ Homework/HW2/js/script.js
@@ -1,13 +1,49 @@
+//Event Listener
 document.querySelector("button").addEventListener("click", gradeQuiz);
 
+//Global variables
+var score = 0;
+var attempts = localStorage.getItem("total_attempts");
+
+
+
+
+displayQ4Choices();
+
+//Functions
+function displayQ4Choices() {
+  let q4ChoicesArray = ["Maine", "Rhode Island", "Maryland", "Delaware"];
+  q4ChoicesArray = _.shuffle(q4ChoicesArray);
+  for (let i = 0; i < q4ChoicesArray.length; i++) {
+    document.querySelector("#q4Choices").innerHTML += 
+      `<input type="radio" name="q4" id="${q4ChoicesArray[i]}" 
+       value="${q4ChoicesArray[i]}">
+       <label for="${q4ChoicesArray[i]}"> ${q4ChoicesArray[i]}</label><br>`;
+  }
+} //displayQ4Choices
+
+function rightAnswer(index){
+  document.querySelector(`#q${index}Feedback`).innerHTML = "Correct!";
+  document.querySelector(`#q${index}Feedback`).className = "bg-success text-white";
+  document.querySelector(`#markImg${index}`).innerHTML = "<img src='img/checkmark.png'>";
+  score += 20;
+}
+
+function wrongAnswer(index){
+  document.querySelector(`#q${index}Feedback`).innerHTML = "Incorrect!";
+  document.querySelector(`#q${index}Feedback`).className = "bg-warning text-white";
+  document.querySelector(`#markImg${index}`).innerHTML = "<img src='img/xmark.png' alt='xmark'>";
+}
+
+//Validation
 function isFormValid(){
   let isValid = true;
-  if(document.querySelector("#q1").value == ""){
+  if (document.querySelector("#q1").value == "") {
     isValid = false;
     document.querySelector("#validationFdbk").innerHTML = "Question 1 was not answered";
   }
   return isValid;
-} 
+}
 
 function gradeQuiz(){
   console.log("Grading quiz..");
@@ -16,7 +52,51 @@ function gradeQuiz(){
     return;
   }
 
-  let q1Response = document.querySelector("#q1").value;
-  console.log(q1Response);
-} 
+  // Reset score and get responses
+  score = 0;
+  let q1Response = document.querySelector("#q1").value.toLowerCase().trim();
+  let q2Response = document.querySelector("#q2").value;
+  let q4Response = document.querySelector("input[name=q4]:checked").value;
 
+  console.log(q1Response, q2Response);
+
+  //Grading question 1
+  if (q1Response == "sacramento") {
+    rightAnswer(1);
+  } else {
+    wrongAnswer(1);
+  }
+
+  //Grading question 2
+  if (q2Response == "mo") {
+    rightAnswer(2);
+  } else {
+    wrongAnswer(2);
+  }
+
+  //Grading question 3
+  if (document.querySelector("#Jefferson").checked && 
+    document.querySelector("#Roosevelt").checked && 
+    !document.querySelector("#Jackson").checked && 
+    !document.querySelector("#Franklin").checked) {
+  rightAnswer(3);
+  }
+  else {
+  wrongAnswer(3);
+  }
+  //Question 4
+  if (q4Response == "Rhode Island") {
+  rightAnswer(4);
+  }
+  else {
+  wrongAnswer(4);
+  }
+
+
+
+  //Display total
+  document.querySelector("#totalScore").innerHTML = `Total Score: ${score}`;
+  document.querySelector("#totalAttempts").innerHTML = `Total Attempts: ${++attempts}`;
+  localStorage.setItem("total_attempts", attempts); 
+
+}
