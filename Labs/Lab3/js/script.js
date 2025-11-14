82ee4c3a0b01d3475acdfdccb6a32efab5a8138e
angelicaa-g <angelicaghazaryann@gmail.com>
2025-11-14 15:19:13 -0800

    added a function

--- Labs/Lab3/js/script.js
+++ Labs/Lab3/js/script.js
@@ -1,5 +1,9 @@
 document.querySelector("#zip").addEventListener("change", displayCity);
 
-function displayCity(){
-    alert("getting city name")
+async function displayCity(){
+    let zipCode = document.querySelector("#zip").value;
+    let url = `https://csumb.space/api/cityInfoAPI.php?zip=${zipCode}`;
+    let response= await fetch(url);
+    let data = await response.json();
+    document.querySelector("#city").innerHTML = data.city;
 }
