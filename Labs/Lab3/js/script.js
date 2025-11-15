57d60e7860fc514d5f6b4ed8dc608dd30335409e
angelicaa-g <angelicaghazaryann@gmail.com>
2025-11-15 13:18:02 -0800

    added checkUsername

--- Labs/Lab3/js/script.js
+++ Labs/Lab3/js/script.js
@@ -1,4 +1,8 @@
 document.querySelector("#zip").addEventListener("change", displayCity);
+document.querySelector("#state").addEventListener("change", displayCounties);
+document.querySelector("#username").addEventListener("change", checkUsername);
+
+
 
 async function displayCity(){
     let zipCode = document.querySelector("#zip").value;
@@ -8,4 +12,34 @@ async function displayCity(){
     document.querySelector("#city").innerHTML = data.city;
     document.querySelector("#latitude").innerHTML = data.latitude;
     document.querySelector("#longitude").innerHTML = data.longitude;
+}
+
+async function displayCounties() {
+    let state = document.querySelector("#state").value;
+    let url = `https://csumb.space/api/countyListAPI.php?state=${state}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    let countyList = document.querySelector("#county");
+    countyList.innerHTML = ""; 
+
+    for(let i = 0; i < data.length; i++) {
+    countyList.innerHTML += `<option>${data[i].county}</option>`;
+    
+ }
+} 
+
+async function checkUsername(){
+    let username = document.querySelector("#username").value;
+    let url = `https://csumb.space/api/usernamesAPI.php?username=${username}`;
+    let response= await fetch(url);
+    let data = await response.json();
+    let usernameError = document.querySelector("#usernameError");
+
+    if (data.available) {
+        usernameError.innerHTML = "Username available!";
+        usernameError.style.color = "green";
+    } else {
+        usernameError.innerHTML = "Username taken";
+        usernameError.style.color = "red";
+    }
 }
