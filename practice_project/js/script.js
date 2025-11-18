e3a3ad2c2d858367c2ac52339d08ba7185df5f9f
angelicaa-g <angelicaghazaryann@gmail.com>
2025-11-18 15:47:03 -0800

    added css style

--- practice_project/js/script.js
+++ practice_project/js/script.js
@@ -1,3 +1,4 @@
+document.querySelector("#Author_Info").addEventListener("click",displayInfo);
 getQuote();
 
 async function getQuote(){
@@ -6,10 +7,19 @@ async function getQuote(){
     let data = await response.json(); //converts raw data into json format
 
     console.log(data); //displays the data
-    console.log("JS is running!");
+   
 
     document.querySelector("#random_quote").innerHTML =  `${data.quoteText}`;
     document.querySelector("#author_fullName").innerHTML =`${data.firstName} ${data.lastName}`; //displays the corresponding author's first and last name
+}
+async function displayInfo(){
+    let url = "https://csumb.space/api/famousQuotes/getRandomQuote.php";
+    let response = await fetch(url);
+    let data = await response.json(); //converts raw data into json format
+
+    console.log(data); //displays the data
+   document.querySelector("#bioCol").innerHTML = `${data.bio}`;
+   document.querySelector("#imgCol").innerHTML = `<img src=${data.picture} width="200">`; //displays the image with custom sizing
 }
 
 
